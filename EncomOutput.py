d385567211f0c6ab8056b315df303898e1812793
nhlamulokhoza <79716218+nhlamulokhoza@users.noreply.github.com>
2022-02-28 11:01:41 +0200

    Update EncomOutput.py

--- EncomOutput.py
+++ EncomOutput.py
@@ -20,17 +20,17 @@ class Encom:
         eigenvalues = [] ; eigenvectors = []
         for mode in raw_format:
             if "Eigenvalue:" in mode:
-                header_and_data = re.compile('.*Eigenvalue:|\n').split(mode )
+                header_and_data = re.compile('.*Eigenvalue:|\n').split(mode)
                 clean_list = [elem for elem in header_and_data if elem]
                 eigenvalues.append( float(clean_list[0]) )
                 current_eigenvector = []
                 for eigenvector_line in header_and_data[1:]:
                     component = eigenvector_line.split('\t')
                     try :
-                        current_eigenvector.append( float(component[2]) )
+                        current_eigenvector.append(float(component[2]))
                     except:
                         pass
-                eigenvectors.append( current_eigenvector )
+                eigenvectors.append(current_eigenvector)
         self.eigenvalues = eigenvalues
         self.eigenvectors = eigenvectors
 
@@ -47,8 +47,8 @@ def calRMSIP(subspace1, subspace2, n = 50):
         sum_outer = 0
         for eigenvector1 in sub1[6:6+n]: 
             for eigenvector2 in sub2[6:6+n]:
-                sum_outer = sum_outer + (np.dot(np.array( eigenvector1 ), np.array( eigenvector2)) )**2
-        return np.sqrt(np.array(sum_outer) /float(n) ) 
+                sum_outer = sum_outer + (np.dot(np.array(eigenvector1), np.array(eigenvector2)))**2
+        return np.sqrt(np.array(sum_outer) / float(n)) 
     else: 
         raise TypeError ('Arguments must be instances of class Encom')
 
@@ -59,22 +59,22 @@ def getEntropy(wt_subspace, mut_subspace):
         eigen1 = wt_subspace.eigenvalues
         mut_subspace.parse_eigen()
         eigen2 = mut_subspace.eigenvalues
-        assert len(eigen2) == len(eigen1), "Dimensions of the eigenvalues for the mutant and the WT are not the same. They came prbably from different proteins !"
+        assert len(eigen2) == len(eigen1),
         fraction_eigenvalues = np.array(eigen2[6:])/np.array(eigen1[6:])
-        # deltaS_vib 
         return np.log(np.prod(fraction_eigenvalues))
 
 # load input files. CYP2F1 was used here 
-mut_encom = Encom( "CYP2F1_3.001.eigen" )
+mut_encom = Encom("CYP2F1_3.001.eigen")
 mut_encom.parse_eigen()
 mut_eigenvectors = mut_encom.eigenvectors
 mut_eigenvalues = mut_encom.eigenvalues
-wt_encom = Encom( "CYP2F1_WT.eigen" )
+wt_encom = Encom("CYP2F1_WT.eigen")
 wt_encom.parse_eigen()
 mut_eigenvectors = wt_encom.eigenvectors
 mut_eigenvalues = wt_encom.eigenvalues
-rmsip=""
-entropy = str( round(getEntropy(mut_encom, wt_encom), 3))
+rmsip = str(calRMSIP(mut_encom, wt_encom))
+entropy = str(round(getEntropy(mut_encom, wt_encom), 3))
 
 # print change in entropy between wildtype and mutant.
-print(entropy)
+print("entropy =", entropy, "kcal/mol")
+print("rmsip =", rmsip)
